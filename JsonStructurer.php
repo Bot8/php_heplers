6cf1db10e2d3828d323fc611a455d9c2c6fe0907
artarn <artarn@yandex.ru>
2017-12-12 11:36:30 +0300

    JsonStructurer

---
+++ JsonStructurer.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Class JsonTester
+ */
+class JsonTester
+{
+    /** @var array  */
+    protected $structure = [];
+
+    /** @var \Closure[]  */
+    protected $skipKeyCallbacks;
+
+    public function getStructure(): array
+    {
+        return $this->structure;
+    }
+
+    /**
+     * JsonTester constructor.
+     *
+     * @param array $data
+     * @param array $skipKeyCallbacks
+     */
+    public function __construct(array $data, array $skipKeyCallbacks = [])
+    {
+        $this->skipKeyCallbacks = $skipKeyCallbacks;
+
+        $this->parse($data);
+    }
+
+    protected function skipKey($key)
+    {
+        if (is_numeric($key)) {
+            return true;
+        }
+
+        foreach ($this->skipKeyCallbacks as $callback) {
+            if ($callback($key)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function parse($array, $arrayKey = '')
+    {
+        foreach ($array as $key => $value) {
+
+            if ($this->skipKey($key)) {
+                $key = '*';
+            }
+
+            $itemKey = empty($arrayKey) ? $key : "$arrayKey.$key";
+
+            if (is_array($value) && !empty($value)) {
+                $this->parse($value, $itemKey);
+            } else {
+                $this->put($arrayKey, $key);
+            }
+        }
+    }
+
+    protected function put($storageKey, $value)
+    {
+        if (!isset($this->structure[$storageKey])) {
+            $this->structure[$storageKey] = [$value];
+        } else {
+            if (!in_array($value, $this->structure[$storageKey])) {
+                $this->structure[$storageKey][] = $value;
+            }
+        }
+    }
+}
